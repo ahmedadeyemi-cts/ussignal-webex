0ba01c2b551cb42db677830c64b99471f3b4029a
ahmedadeyemi-cts <ahmedadeyemi@gmail.com>
2026-02-07 13:39:25 -0800

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,13 @@
+export async function getUserContext(jwt) {
+  const payload = JSON.parse(
+    atob(jwt.split(".")[1])
+  );
+
+  const email = payload.email?.toLowerCase();
+  if (!email) throw new Error("Email missing from Access token");
+
+  return {
+    email,
+    role: email.endsWith("@ussignal.com") ? "admin" : "customer"
+  };
+}
