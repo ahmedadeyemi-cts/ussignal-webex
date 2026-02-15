facdd27b404fd2152585695494be77c6981411c6
ahmedadeyemi-cts <ahmedadeyemi@gmail.com>
2026-02-14 21:41:58 -0800

    Add shell.js for rendering shell UI components

---
+++ ui/shared/shell.js
@@ -0,0 +1,9 @@
+<script src="https://raw.githubusercontent.com/ahmedadeyemi-cts/ussignal-webex/main/ui/shared/shell.js"></script>
+<script>
+  // After loadMe(), call:
+  window.renderShell({
+    role: ME.role,
+    orgName: ME.orgName,
+    activePath: location.pathname
+  });
+</script>
