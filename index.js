4ad0828cec00f15c851818ef7aac37dd5a8d7fad
ahmedadeyemi-cts <ahmedadeyemi@gmail.com>
2026-03-09 17:10:10 -0400

    Update index.js

--- index.js
+++ index.js
@@ -590,15 +590,11 @@ function sleep(ms) {
 // =====================================================
 
 function chunkArray(arr, size) {
-
   const chunks = [];
-
   for (let i = 0; i < arr.length; i += size) {
     chunks.push(arr.slice(i, i + size));
   }
-
   return chunks;
-
 }
 // =====================================================
 // OBSERVABILITY STREAM ENGINE
