93fbf8c52ed2ab41a7eb4ad8473cc2f1110e5225
ahmedadeyemi-cts <ahmedadeyemi@gmail.com>
2026-03-19 17:00:50 -0700

    Update index.js

--- index.js
+++ index.js
@@ -202,7 +202,7 @@ async function createPartnerReport(env) {
 
   return data.reportId;
 }
-async function pollPartnerReports(env, orgs)
+async function pollPartnerReports(env)
 
   const list = await env.WEBEX.list({ prefix: "cdr:report:" });
 
